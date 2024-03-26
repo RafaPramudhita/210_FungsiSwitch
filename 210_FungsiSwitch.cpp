ee05fc80c2ad75677c1dd3257783d623e4d3e2de
RafaPramudhita <161433424+RafaPramudhita@users.noreply.github.com>
2024-03-26 15:15:33 +0700

    membuat input pilihan

--- 210_FungsiSwitch.cpp
+++ 210_FungsiSwitch.cpp
@@ -16,7 +16,7 @@ int penjumlahan( int a, int b)
     return a + b;
 }
 
-int penguranngan( int a, int b)
+int pengurangan( int a, int b)
 {
     return a - b;
 }
@@ -31,3 +31,21 @@ float pembagian(int a, int b)
     return a / b;
 }
 
+int main()
+{
+    int pilihan;
+
+    do{
+        cout << "Kalkulator sederhana" << endl;
+        cout << "====================" << endl;
+        cout << "1. Penjumlahan" << endl;
+        cout << "2. Pengurangan" << endl;
+        cout << "3. Perkalian" << endl;
+        cout << "4. Pembagian" << endl;
+        cout << "5. Keluar" << endl;
+        cout << "====================" << endl;
+        cout << "Masukkan menu pilihan : " ;
+        cin >> pilihan;
+    }while (pilihan !=5);
+        
+}
